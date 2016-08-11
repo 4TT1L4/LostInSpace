3800dc7dd8448bbebd7370eb5ec05adf814e7b48
Attila Bujáki <Bujaki.Attila@gmail.com>
2016-08-11 22:41:13 +0200

    Added javadoc to Entrance and Exit classes

--- LostInSpace/src/main/java/LostInSpace/LevelGenerator/Entrance.java
+++ LostInSpace/src/main/java/LostInSpace/LevelGenerator/Entrance.java
@@ -1,5 +1,11 @@
 package LostInSpace.LevelGenerator;
 
+/**
+ * Special node for the entrance node.
+ * 
+ * @author Attila Bujáki
+ *
+ */
 public class Entrance extends Node {
 	public String toString() {
 		return "EntranceNode";
--- LostInSpace/src/main/java/LostInSpace/LevelGenerator/Exit.java
+++ LostInSpace/src/main/java/LostInSpace/LevelGenerator/Exit.java
@@ -1,5 +1,11 @@
 package LostInSpace.LevelGenerator;
 
+/**
+ * Special node for the Exit node.
+ * 
+ * @author Attila Bujáki
+ *
+ */
 public class Exit extends Node{
 	@Override
 	public String toString() {
